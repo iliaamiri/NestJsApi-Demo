4fdeb33f5738a8f06b1e26cb93b19376a03f6a8b
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-06-18 16:38:59 -0700

    Create PokerVancApiAuthorizationHeadersFields.ts

---
+++ src/Models/StaticModels/PokerVancApiAuthorizationHeadersFields.ts
@@ -0,0 +1,8 @@
+export default abstract class PokerVancApiAuthorizationHeaders {
+  public static readonly FieldNames = {
+    ProviderId: 'PokerVanc-ProviderId',
+    ProviderUsername: 'PokerVanc-Username',
+    RequestType: 'PokerVanc-RequestType',
+    ProviderSignature: 'PokerVanc-ProviderSignature',
+  };
+}
